398a068798c9ff2855955afe31f4c19b02c6e656
sanchezsan2809 <santiasanchez@frba.utn.edu.ar>
2025-11-18 20:59:10 -0300

    ADD: Población de tabla de hechos de PAGOS

--- script_creacion_BI.sql
+++ script_creacion_BI.sql
@@ -64,7 +64,6 @@ CREATE TABLE GRUPO_43.bi_dim_curso(
 	id_dim_curso INT IDENTITY PRIMARY KEY, 
 	curso_codigo CHAR(8) NOT NULL, 
 	categoria CHAR(8) NOT NULL,
-	precio decimal(8,2) NOT NULL
 ); 
 
 
@@ -102,10 +101,6 @@ CREATE TABLE GRUPO_43.bi_facto_cursadas(
 	id_dim_curso INT NOT NULL,
 	id_dim_alumno INT NOT NULL, 
 	id_dim_profesor INT NOT NULL, 
-	nota_tp BIGINT,
-	nota_modulo_1 BIGINT,
-	nota_modulo_2 BIGINT, 
-	nota_modulo_3 BIGINT, 
 	estado_cursada BIT,
 	FOREIGN KEY(id_dim_tiempo_inicio) REFERENCES GRUPO_43.bi_dim_tiempo,
 	FOREIGN KEY(id_dim_tiempo_finalizacion) REFERENCES GRUPO_43.bi_dim_tiempo, 
@@ -122,7 +117,7 @@ CREATE TABLE GRUPO_43.bi_facto_finales(
 	id_dim_curso INT NOT NULL,
 	id_dim_alumno INT NOT NULL,
 	nota_final BIGINT,
-	estado_final BIT, 
+	presencia_final BIT NOT NULL,
 	FOREIGN KEY(id_dim_tiempo) REFERENCES GRUPO_43.bi_dim_tiempo, 
 	FOREIGN KEY(id_dim_sede) REFERENCES GRUPO_43.bi_dim_sede,
 	FOREIGN KEY(id_dim_curso) REFERENCES GRUPO_43.bi_dim_curso, 
@@ -286,12 +281,11 @@ BEGIN
 
 	INSERT INTO GRUPO_43.bi_dim_curso(
 		curso_codigo, 
-		categoria,
-		precio
+		categoria
 	)
 	SELECT DISTINCT
 		curso_codigo, 
-		detalle_curso_categoria 
+		detalle_curso_categoria
 	FROM GRUPO_43.curso
 	JOIN GRUPO_43.detalle_curso ON curso_detalle_curso_id = detalle_curso_id
 END
@@ -323,5 +317,177 @@ BEGIN
 	SELECT DISTINCT ISNULL(turno_descripcion, 'No especificado')
 	FROM GRUPO_43.turno
 END
+GO
 
+--	Procedures para hechos
+CREATE OR ALTER PROCEDURE GRUPO_43.bi_facto_inscripciones
+AS
+BEGIN
+    SET NOCOUNT ON; 
+    DELETE FROM GRUPO_43.bi_facto_inscripciones; 
 
+    INSERT INTO GRUPO_43.bi_facto_inscripciones(
+        id_dim_tiempo_inscripcion, 
+        id_dim_sede, 
+        id_dim_curso, 
+        id_dim_alumno, 
+        id_dim_turno, 
+        estado_inscripcion
+    )
+    SELECT
+        t.id_dim_tiempo,
+        s.id_dim_sede, 
+        c.id_dim_curso,
+        a.id_dim_alumno, 
+        tu.id_dim_turno,
+        i.inscrip_curso_estado
+    FROM GRUPO_43.inscripcion_curso i
+    JOIN GRUPO_43.bi_dim_tiempo t 
+        ON t.fecha = i.inscrip_curso_fecha
+    JOIN GRUPO_43.curso co 
+        ON co.curso_codigo = i.inscrip_curso_codigo
+    JOIN GRUPO_43.bi_dim_sede s 
+        ON s.sede_id = co.curso_sede_id
+    JOIN GRUPO_43.bi_dim_curso c 
+        ON c.curso_codigo = co.curso_codigo
+    JOIN GRUPO_43.bi_dim_alumno a 
+        ON a.alumno_legajo = i.inscrip_curso_alumno_legajo
+    JOIN GRUPO_43.turno t2
+        ON t2.turno_id = co.curso_turno_id
+    JOIN GRUPO_43.bi_dim_turno tu
+        ON tu.turno = t2.turno_descripcion;
+END
+GO
+
+CREATE OR ALTER PROCEDURE GRUPO_43.bi_facto_cursadas AS
+BEGIN
+    SET NOCOUNT ON; 
+    DELETE FROM GRUPO_43.bi_facto_cursadas; 
+
+    INSERT INTO GRUPO_43.bi_facto_cursadas(
+        id_dim_tiempo_inicio, 
+        id_dim_tiempo_finalizacion, 
+        id_dim_sede, 
+        id_dim_curso, 
+        id_dim_alumno, 
+        id_dim_profesor, 
+        estado_cursada
+    )
+    SELECT
+        t_ini.id_dim_tiempo, 
+        t_fin.id_dim_tiempo, 
+        sdim.id_dim_sede, 
+        cdim.id_dim_curso,
+        adim.id_dim_alumno, 
+        prodim.id_dim_profesor,
+        CASE 
+            WHEN ISNULL(tp.tp_nota,0) >= 4 
+                 AND ISNULL(eval.nota_minima,0) >= 4 
+            THEN 1 ELSE 0
+        END AS estado_cursada
+    FROM GRUPO_43.inscripcion_curso ic 
+    JOIN GRUPO_43.curso c 
+        ON c.curso_codigo = ic.inscrip_curso_codigo
+    JOIN GRUPO_43.alumno a 
+        ON a.alumno_legajo = ic.inscrip_curso_alumno_legajo
+    
+    -- Dimensiones
+    JOIN GRUPO_43.bi_dim_alumno adim 
+        ON adim.alumno_legajo = a.alumno_legajo
+    JOIN GRUPO_43.bi_dim_curso cdim 
+        ON cdim.curso_codigo = c.curso_codigo
+    JOIN GRUPO_43.bi_dim_profesor prodim 
+        ON prodim.profesor_id = c.curso_profesor_id
+    JOIN GRUPO_43.bi_dim_sede sdim 
+        ON sdim.sede_id = c.curso_sede_id
+    JOIN GRUPO_43.bi_dim_tiempo t_ini 
+        ON t_ini.fecha = c.curso_fecha_inicio
+    JOIN GRUPO_43.bi_dim_tiempo t_fin 
+        ON t_fin.fecha = c.curso_fecha_fin
+
+    -- Nota TP
+    LEFT JOIN GRUPO_43.tp tp
+        ON tp.tp_alumno_legajo = a.alumno_legajo
+        AND tp.tp_curso_codigo = c.curso_codigo
+        AND tp.tp_fecha_evaluacion BETWEEN c.curso_fecha_inicio AND c.curso_fecha_fin
+
+    -- Evaluaciones (nota mínima en rango de fechas)
+    CROSS APPLY (
+        SELECT MIN(e.evaluacion_nota) AS nota_minima
+        FROM GRUPO_43.evaluacion e
+        WHERE e.evaluacion_alumno_legajo = a.alumno_legajo
+          AND e.evaluacion_curso_id = c.curso_codigo
+          AND e.evaluacion_fecha BETWEEN c.curso_fecha_inicio AND c.curso_fecha_fin
+    ) eval;
+END;
+GO
+
+CREATE OR ALTER PROCEDURE bi_facto_finales
+AS
+BEGIN
+	SET NOCOUNT ON;
+	DELETE FROM GRUPO_43.bi_facto_finales; 
+
+	INSERT INTO GRUPO_43.bi_facto_finales(
+		id_dim_tiempo,
+		id_dim_sede, 
+		id_dim_curso, 
+		id_dim_alumno, 
+		nota_final, 
+		presencia_final
+	)
+	SELECT
+		t.id_dim_tiempo,
+		s.id_dim_sede,
+		cu.id_dim_curso,
+		a.id_dim_alumno,
+		f.final_nota,
+		f.final_presente
+	FROM GRUPO_43.evaluacion_final f
+	JOIN GRUPO_43.instancia_final ins_f ON ins_f.instancia_final_id = f.final_instancia_final
+	JOIN GRUPO_43.curso c ON ins_f.instancia_final_curso = c.curso_codigo
+	JOIN GRUPO_43.bi_dim_tiempo t ON t.fecha = ins_f.instancia_final_fecha
+	JOIN GRUPO_43.bi_dim_sede s ON s.sede_id = c.curso_sede_id
+	JOIN GRUPO_43.bi_dim_curso cu ON cu.curso_codigo = c.curso_codigo
+	JOIN GRUPO_43.bi_dim_alumno a ON a.alumno_legajo = f.final_alumno_legajo
+END
+GO 
+
+CREATE OR ALTER PROCEDURE GRUPO_43.bi_facto_pagos
+AS
+BEGIN
+	SET NOCOUNT ON;
+	DELETE FROM GRUPO_43.bi_facto_pagos; 
+
+	INSERT INTO GRUPO_43.bi_facto_pagos(
+		id_dim_tiempo,
+		id_dim_sede,
+		id_dim_medio_pago,
+		id_dim_curso,
+		id_dim_tiempo_vencimiento,
+		id_dim_tiempo_pago,
+		monto_facturado,
+		monto_pagado,
+		estado_pago
+	)
+	SELECT
+		tf.id_dim_tiempo,
+		s.id_dim_sede,
+		mp.id_dim_medio_pago,
+		cu.id_dim_curso,
+		tv.id_dim_tiempo,
+		tp.id_dim_tiempo,
+		f.fact_importe_total,
+		p.pago_importe,
+		CASE WHEN p.pago_importe = f.fact_importe_total THEN 1 ELSE 0 END 
+	FROM GRUPO_43.pago p
+	JOIN GRUPO_43.factura f ON f.fact_nro = p.pago_fact_id 
+	JOIN GRUPO_43.detalle_factura df ON df.detalle_factura_fact_id = f.fact_nro
+	JOIN GRUPO_43.curso c ON c.curso_codigo = df.detalle_factura_curso_id 
+	JOIN GRUPO_43.bi_dim_tiempo tf ON tf.fecha = f.fact_fecha_emision
+	JOIN GRUPO_43.bi_dim_sede s ON s.sede_id = c.curso_sede_id
+	JOIN GRUPO_43.bi_dim_medio_pago mp ON mp.tipo_medio_pago = p.pago_medio_de_pago
+	JOIN GRUPO_43.bi_dim_curso cu ON cu.curso_codigo = c.curso_codigo
+	JOIN GRUPO_43.bi_dim_tiempo tv ON tv.fecha = f.fact_fecha_venc
+	JOIN GRUPO_43.bi_dim_tiempo tp ON tp.fecha = p.pago_fecha
+END
