9d04cb438a364607a5854fe1549c6c5d6f5ea3c9
sanchezsan2809 <santiasanchez@frba.utn.edu.ar>
2025-12-01 20:38:04 -0300

    ADD: View para Índice de satisfacción

--- script_creacion_BI.sql
+++ script_creacion_BI.sql
@@ -895,20 +895,24 @@ CREATE OR ALTER VIEW GRUPO_43.indice_satisfaccion_anual
 AS
 SELECT
 	t.anio AÑO, 
-	p.rango_etario RANGO_ETARIO,
-	se.sede_id SEDE,
+	rep.rango_etario RANGO_ETARIO,
+	se.nombre SEDE,
 	(
-		(
-			(CAST(COUNT(CASE WHEN s.bloque_satisfaccion = 2 THEN 1 END) AS float) / COUNT(*)) 
-			- 
-			(CAST(COUNT(CASE WHEN s.bloque_satisfaccion = 0 THEN 1 END) AS float) / COUNT(*))
-		) + 100
-	) / 2 AS INDICE_SATISFACCION
+		(SUM(s.satisfechos) * 1.0 / SUM(s.satisfechos + s.neutrales + s.insatisfechos) * 100)
+		-
+		(SUM(s.insatisfechos) * 1.0 / SUM(s.satisfechos + s.neutrales + s.insatisfechos) * 100)
+	+100) / 2 INDICE_SATISfACCION 
 FROM GRUPO_43.bi_facto_satisfaccion s
-JOIN GRUPO_43.bi_dim_profesor p ON p.id_dim_profesor = s.id_dim_profesor
-JOIN GRUPO_43.bi_dim_sede se ON se.id_dim_sede = s.id_dim_sede
-JOIN GRUPO_43.bi_dim_tiempo t ON t.id_dim_tiempo = s.id_dim_tiempo
-GROUP BY t.anio, p.rango_etario, se.sede_id
+JOIN GRUPO_43.bi_dim_RE_profesor rep 
+	ON rep.id_dim_RE_profesor = s.id_dim_RE_profesor
+JOIN GRUPO_43.bi_dim_sede se 
+	ON se.id_dim_sede = s.id_dim_sede
+JOIN GRUPO_43.bi_dim_tiempo t 
+	ON t.id_dim_tiempo = s.id_dim_tiempo
+GROUP BY 
+	t.anio, 
+	rep.rango_etario, 
+	se.nombre
 GO
 
 
